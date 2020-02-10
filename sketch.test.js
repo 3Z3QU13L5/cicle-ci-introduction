f73e46233cce063f1e6131860ef4b813acaadb03
Intellisys <intellisys@Intellisyss-MacBook-Pro-4.local>
2020-02-10 10:07:08 -0400

    creating a class and adding more operations

--- sketch.test.js
+++ sketch.test.js
@@ -1,5 +1,21 @@
-const sum = require('./sketch');
+const math = require('./sketch');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(math.sum(1, 2)).toBe(3);
+}); 
+
+test('adds 4 - 1 to equal 3', () => {
+    expect(math.rest(2, 1)).toBe(3);
+}); 
+
+test('adds 1 x 3 to equal 3', () => {
+    expect(math.multiplication(1, 3)).toBe(3);
+}); 
+
+test('adds 9 / 3 to equal 3', () => {
+    expect(math.divide(9, 3)).toBe(3);
+}); 
+
+test('adds 18 % 5 to equal 3', () => {
+    expect(math.residue(18, 5)).toBe(3);
 }); 
